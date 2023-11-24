3979ef9853b189d02fc4b69a6433d4468a78e6c8
MiguelRamos <miguelramostapia@gmail.com>
2023-11-24 12:18:42 -0600

    arrays metodos

--- assets/js/index_resumen.js
+++ assets/js/index_resumen.js
@@ -8,7 +8,7 @@ const listado_tareas = [
     {
         id: 2,
         descripcion: 'Tarea número 2',
-        realizado: true,
+        realizado: false,
         eliminado: false
     },
     {
@@ -67,33 +67,59 @@ function add_tarea(){
     listado_tareas.push({id:cantidad_tareas,descripcion:value_test,realizado:false,eliminado:false})
     refresh_pagina_inicio()
 }
+function eliminar_tarea(id_tarea){
+    for (let item_tarea of listado_tareas){
+        if(item_tarea.id==id_tarea){
+            item_tarea.eliminado=true
+        }
+    }
+    refresh_pagina_inicio()
+}
+function tarea_realizada(id_tarea){
+    for (let item_tarea of listado_tareas){
+        if(item_tarea.id==id_tarea){
+            item_tarea.realizado=true
+        }
+    }
+    refresh_pagina_inicio()
+}
 
 function refresh_pagina_inicio(){
     const texto_html=document.querySelector(".resumen")
     let texto_de_paso=""
     let cantidad_tareas = listado_tareas.length
     let cantidad_tareas_realizadas=0
+    let cantidad_tareas_eliminadas=0
     for (let item_tarea of listado_tareas){
         if(item_tarea.realizado==true && item_tarea.eliminado==false){
             cantidad_tareas_realizadas=cantidad_tareas_realizadas+1
         }
+        if(item_tarea.eliminado==true){
+            cantidad_tareas_eliminadas=cantidad_tareas_eliminadas+1;
+        }
     }
-    texto_de_paso=`<p>Total : ${cantidad_tareas}</p><br>
-                <p>Realizadas : ${cantidad_tareas_realizadas}</p>`
+    texto_de_paso=`<p>Total de Tareas : ${cantidad_tareas}</p><br>
+                <p>Realizadas : ${cantidad_tareas_realizadas}</p><br>
+                <p>Eliminadas : ${cantidad_tareas_eliminadas}</p>`
     texto_html.innerHTML=texto_de_paso
 
     const texto_html_tarea=document.querySelector(".tareas")
     let texto_de_tareas="ID     DESCRIPCION          REALIZADA"
     for (let item_tarea of listado_tareas){
         if(item_tarea.eliminado==false){
-            texto_de_tareas+=
-            `<p>${item_tarea.id}    ${item_tarea.descripcion}    <input type=checkbox>${item_tarea.realizado}  <i class="fa-regular fa-trash-can fa-lg"></i>  ${item_tarea.eliminado}</p>`
+            if(item_tarea.realizado==false){
+                texto_de_tareas+=
+                `<p>${item_tarea.id}   ${item_tarea.descripcion}    <input type=checkbox id="do_it" onclick="tarea_realizada(${item_tarea.id})"><button onclick="eliminar_tarea(${item_tarea.id})">             <i class="fa-regular fa-trash-can fa-lg"></i></button></p>`    
+            }else{
+                texto_de_tareas+=
+                `<p>${item_tarea.id}   ${item_tarea.descripcion}    <input type=checkbox id="do_it" checked> <button onclick="eliminar_tarea(${item_tarea.id})">                <i class="fa-regular fa-trash-can fa-lg"></i></button></p>`
             }
+        }
     }
     texto_html_tarea.innerHTML=texto_de_tareas
-    var value_test = document.querySelector(".uname").value;
+/*    var value_test = document.querySelector(".uname").value;
     console.log(value_test)
     value_test=""
     value_test.innerHTML=""
-    console.log(value_test)
+    console.log(value_test)*/
 }
